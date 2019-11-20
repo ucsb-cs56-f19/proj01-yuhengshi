641bc87ba6aa3131b5a417de2b9f6535df3b951d
yuhengshi@umail.ucsb.edu <yuhengshi@umail.ucsb.edu>
2019-11-20 11:58:24 -0800

    added the featurecollection and also fixed webcontroller

--- src/main/java/hello/WebController.java
+++ src/main/java/hello/WebController.java
@@ -1,5 +1,7 @@
 package hello;
 
+import hello.geojson.FeatureCollection;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
@@ -59,10 +61,15 @@ public class WebController {
     @GetMapping("/earthquakes/results")
     public String getEarthquakesResults(Model model, OAuth2AuthenticationToken oAuth2AuthenticationToken,
             EqSearch eqSearch) {
-        EarthquakeQueryService e = new EarthquakeQueryService();
-        model.addAttribute("eqSearch", eqSearch);
-        String json = e.getJSON(eqSearch.getDistance(), eqSearch.getMinmag());
-        model.addAttribute("json", json);
-        return "earthquakes/results";
+
+            EarthquakeQueryService e =
+            new EarthquakeQueryService();
+
+            model.addAttribute("eqSearch", eqSearch);
+            String json = e.getJSON(eqSearch.getDistance(), eqSearch.getMinmag());
+            model.addAttribute("json", json);
+             FeatureCollection featureCollection = FeatureCollection.fromJSON(json);
+            model.addAttribute("featureCollection",featureCollection);
+            return "earthquakes/results";
     }
 }
