60342d59e0dafc0b6e1ef0c4e592793e32329b13
yuhengshi@umail.ucsb.edu <yuhengshi@umail.ucsb.edu>
2019-12-03 13:52:57 -0800

    ys - update javadoc and jacoco report

---
+++ docs/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"earthquakes"},{"l":"earthquakes.controllers"},{"l":"earthquakes.entities"},{"l":"earthquakes.geojson"},{"l":"earthquakes.osm"},{"l":"earthquakes.repositories"},{"l":"earthquakes.searches"},{"l":"earthquakes.services"}]
---
+++ docs/testapidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"earthquakes","l":"BootstrapLiterals"},{"p":"earthquakes","l":"HomePageTest"}]
